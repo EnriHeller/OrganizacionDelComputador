55f919a1f7028fd32452eedec720cdac787d7c18
EnriHeller <enrique.heller12@gmail.com>
2024-06-19 16:07:24 -0300

    Ejercicio 3 perfecto

--- ejercicios/ej3.asm
+++ ejercicios/ej3.asm
@@ -1,23 +1,39 @@
 ;Realizar un programa que resuelva X Y teniendo en cuenta que tanto X e Y pueden
 ;ser positivos o negativos.
-bits 64;
+bits 64
 global main
 extern puts
 extern gets
+extern sscanf
+extern printf
 
 %macro mPuts 1
     mov rdi, %1
-
     sub rsp, 8
     call puts
     add rsp, 8
-
 %endmacro
 
-
-%macro mGets 0
+%macro mGets 1
+    mov rdi, %1
     sub rsp, 8
     call gets
+    add rsp, 8
+%endmacro
+
+%macro mScanNum 2
+    mov rdi, %1
+    mov rsi, numFormat
+    mov rdx, %2
+
+    sub rsp, 8
+    call sscanf
+    add rsp, 8
+%endmacro
+
+%macro mPrintf 0
+    sub rsp, 8
+    call printf
     add rsp, 8
 %endmacro
 
@@ -25,28 +41,52 @@ section .data
     mensajeX db "Ingrese X", 0
     mensajeY db "Ingrese Y", 0
 
+    numFormat   db "%li",0
+    resFormat   db "Resultado: %li",10,0
+
 section .bss
-    X resb 3
-    Y resb 3
+    Xs resb 20
+    Ys resb 20
+
+    Xn resq 1
+    Yn resq 1
+
+    res resq 1
+
 
 section .text
 
 main:
     mPuts mensajeX
-    mov rdi, X 
-    mGets
+    mGets Xs
+
+    escanearX:
+    mScanNum Xs, Xn
 
     mPuts mensajeY
-    mov rdi, Y
-    mGets
+    mGets Ys
 
-    mov ecx, [Y]  ; Cantidad de veces que se desea repetir la instrucción
-repetir:
-    ; Tu instrucción aquí
-    ; Por ejemplo:
-    inc eax     ; Incrementar el valor en el registro eax
-    
-    loop repetir  ; Decrementa ecx y salta a 'repetir' si ecx no es cero
+    escanearY:
+    mScanNum Ys, Yn
 
+    mov cx, 2   
+    mov rax, qword [Xn]
+    mov qword [res], rax
+
+    mov cx, [Yn]  ; Cantidad de veces que se desea repetir la instrucción
+
+    sub rax, rax
+    mov rax, 1
+
+multSucesiva:
+    imul rax, qword [Xn]
+    mov qword [res], rax
+    loop multSucesiva  ; Decrementa ecx y salta a 'repetir' si ecx no es cero
+
+impresion:
+    mov rdi, resFormat
+    mov rsi, qword[res]
+
+    mPrintf
 
     ret
